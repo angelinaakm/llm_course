bf99d79316d3e6a9984f36b341fd0d74c67b5ae2
Angelina Kim <ang.km777@gmail.com>
2023-08-10 16:17:52 +0100

    add sidebar streamlit

--- frontend_app/chat_with_documents.py
+++ frontend_app/chat_with_documents.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
-import os
 from typing import List
 
 # loading PDF, DOCX and TXT files as LangChain Documents
@@ -44,3 +43,40 @@ def chunk_data(data, chunk_size=256, chunk_overlap=20):
 def create_embeddings(chunks):
     embeddings = OpenAIEmbeddings()
     vector_store = Chroma.from_documents(chunks, embeddings)
+    return vector_store
+
+def ask_and_get_answer(vector_store, q, k=3): #higher k is more expensive
+    from langchain.chains import RetrievalQA
+    from langchain.chat_models import ChatOpenAI
+
+    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.2)
+    retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': k})
+    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+
+    answer = chain.run(q)
+    return answer
+
+def return_embedding_cost(texts):
+    import tiktoken
+    enc = tiktoken.encoding_for_model('text-embedding-ada-002')
+    total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
+    return total_tokens, total_tokens/1000 * 0.0004
+
+
+if __name__ == "__main__":
+    import os
+    from dotenv import load_dotenv, find_dotenv
+    load_dotenv(find_dotenv(), override=True)
+
+    #st.image('img.png')
+    st.subheader('LLM Question-Answering Application')
+    with st.sidebar:
+        api_key=st.text_input('OpenAI API Key:', type='password')
+        if api_key:
+            os.environ['OPENAI_API_KEY'] = api_key
+
+        uploaded_file = st.file_uploader('Upload a file ', type=['pdf', 'docs', 'txt'])
+        chunk_size = st.number_input('Chunk size: ', min_value=100, max_value=2048, value=512)
+        k = st.number_input('k', min_value=1, max_value=20, value=3)
+        add_data = st.button ('Add Data')
+    
